04feb156af7a7e707c7b05ca86d5c141f42b06d4
AhmedAdly <157370741+ahadlyy@users.noreply.github.com>
2024-09-20 15:56:57 +0300

    initial commit

---
+++ Employee.vb
@@ -0,0 +1,46 @@
+﻿Public Class Employee
+
+    Private _name As String
+    Private _id As Integer
+    Private _salary As Decimal
+
+
+    Public Sub New(name As String, id As Integer, salary As Decimal)
+        _name = name
+        _id = id
+        _salary = salary
+    End Sub
+
+    Public Property Name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+
+    End Property
+
+    Public Property Id As Integer
+        Get
+            Return _id
+        End Get
+        Set(value As Integer)
+            _id = value
+        End Set
+    End Property
+
+    Public Property Salary As Decimal
+        Get
+            Return _salary
+        End Get
+        Set(value As Decimal)
+            _salary = value
+        End Set
+    End Property
+
+    Public Overridable Function GetDetails() As String
+        Return $"Employee name: {_name}, Employee id: {_id}, Employee salary: {_salary}"
+    End Function
+
+End Class
